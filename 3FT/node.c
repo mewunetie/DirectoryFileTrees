d8dfb65e679d47c9de430815184958cc46645843
Shruti Roy <sr0005@princeton.edu>
2021-11-10 17:07:17 -0500

    new node files

---
+++ 3FT/node.c
@@ -0,0 +1,499 @@
+/*--------------------------------------------------------------------*/
+/* node.c                                                             */
+/* Authors: Misrach Ewunetie, Shruti Roy                              */
+/*--------------------------------------------------------------------*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "dynarray.h"
+#include "node.h"
+
+/*
+   A node structure represents a directory in the directory tree
+*/
+struct node {
+   /* the full path of this directory */
+   char* path;
+
+   /* the parent directory of this directory
+      NULL for the root of the directory tree */
+   Node_T parent;
+
+   /* the files of this directory
+      stored in sorted order by pathname */
+   DynArray_T fchildren;
+
+   /* the subdirectories of this directory
+      stored in sorted order by pathname */
+   DynArray_T dchildren;
+};
+
+/*
+   A File structure represents a directory in the directory tree
+*/
+struct file {
+   /* the full path of this directory */
+   char* path;
+
+   /* the parent directory of this directory
+      NULL for the root of the directory tree */
+   Node_T parent;
+
+   /* content contained in of the file File */
+   void *contents;
+
+   /* size of contents */
+   size_t length;
+};
+
+
+
+/* see node.h for specification */
+Node_T Node_create(const char* dir, Node_T parent, int *status){
+   Node_T new;
+   char* path;
+
+   assert(dir != NULL);
+
+   new = malloc(sizeof(struct node));
+   if(new == NULL) {
+      *status = MEMORY_ERROR;
+      return NULL;
+   }
+
+   if(parent == NULL)
+      path = malloc(strlen(dir)+1);
+   else
+      path = malloc(strlen(parent->path) + 1 + strlen(dir) + 1);
+
+   if(path == NULL) {
+      free(new);
+      *status = MEMORY_ERROR;
+      return NULL;
+   }
+
+   *path = '\0';
+
+   if(parent != NULL) {
+      strcpy(path, parent->path);
+      strcat(path, "/");
+   }
+   strcat(path, dir);
+
+   new->path = path;
+
+   new->parent = parent;
+
+   if(new->fchildren == NULL || new->dchildren == NULL) {
+      if (new->dchildren != NULL) {
+          DynArray_free(new->dchildren);
+      }
+      else if (new->fchildren != NULL) {
+          DynArray_free(new->fchildren);
+      }
+      free(new->path);
+      free(new);
+      *status = MEMORY_ERROR;
+      return NULL;
+   }
+
+   /* if (parent != NULL) {
+      result = Node_linkChild(parent, new);
+      if(result != SUCCESS)
+         Node_destroy(new);
+      *status = result;
+      } */
+
+   return new;
+}
+
+/* see node.h for specification */
+size_t Node_destroy(Node_T n) {
+   size_t i;
+   size_t count = 0;
+   Node_T d;
+   File_T f;
+   
+   assert(n != NULL);
+   
+   /* if (n->parent != NULL) {
+       Node_unlinkChild(n->parent, n);
+       } */
+
+   for(i = 0; i < DynArray_getLength(n->fchildren); i++)
+   {
+      f = DynArray_get(n->fchildren, i);
+      File_destroy(f);
+      count++;
+   }
+   DynArray_free(n->fchildren);
+
+   for(i = 0; i < DynArray_getLength(n->dchildren); i++)
+   {
+      d = DynArray_get(n->dchildren, i);
+      count += Node_destroy(d);
+   }
+   DynArray_free(n->dchildren);
+
+   free(n->path);
+   free(n);
+   count++;
+
+   return count;
+}
+
+/* see node.h for specification */
+char* Node_getPath(Node_T n) {
+   char* pathCopy;
+
+   assert(n != NULL);
+
+   pathCopy = malloc(strlen(n->path)+1);
+   if(pathCopy == NULL)
+      return NULL;
+
+   return strcpy(pathCopy, n->path);
+}
+
+
+/* see node.h for specification */
+int Node_compare(Node_T node1, Node_T node2) {
+   assert(node1 != NULL);
+   assert(node2 != NULL);
+
+   return strcmp(node1->path, node2->path);
+}
+
+/* see node.h for specification */
+size_t Node_getNumChildren(Node_T n, boolean file) {
+   DynArray_T children;
+   assert(n != NULL);
+
+   if(file)
+       children = n->fchildren;
+   else
+       children = n->dchildren;
+   return DynArray_getLength(children);
+}
+
+/* see node.h for specification */
+int Node_hasChild(Node_T n, const char* path, size_t* childID,
+   boolean file) {
+   size_t index;
+   int result;
+   Node_T checker;
+   DynArray_T children;
+
+   assert(n != NULL);
+   assert(path != NULL);
+
+   checker = Node_create(path, NULL, &result);
+   if(checker == NULL) {
+      return -1;
+   }
+
+   if(file)
+        children = n->fchildren;
+    else
+        children = n->dchildren;
+
+   result = DynArray_bsearch(children, checker, &index,
+                    (int (*)(const void*, const void*)) Node_compare);
+   (void) Node_destroy(checker);
+
+   if(childID != NULL)
+      *childID = index;
+
+   return result;
+}
+
+/* see node.h for specification */
+Node_T Node_getDirChild(Node_T n, size_t childID) {
+   assert(n != NULL);
+
+   if(DynArray_getLength(n->dchildren) > childID)
+      return DynArray_get(n->dchildren, childID);
+   else
+      return NULL;
+}
+
+File_T Node_getFileChild(Node_T n, size_t childID) {
+   assert(n != NULL);
+
+   if(DynArray_getLength(n->fchildren) > childID)
+      return DynArray_get(n->fchildren, childID);
+   else
+      return NULL;
+}
+
+/* see node.h for specification */
+Node_T Node_getParent(Node_T n) {
+   assert(n != NULL);
+
+   return n->parent;
+}
+
+/* see node.h for specification */
+int Node_linkChild(Node_T parent, Node_T child) {
+   size_t i;
+   char* rest;
+
+   assert(parent != NULL);
+   assert(child != NULL);
+
+   if(Node_hasChild(parent, child->path, NULL, FALSE))
+      return ALREADY_IN_TREE;
+
+   i = strlen(parent->path);
+   if(strncmp(child->path, parent->path, i))
+      return PARENT_CHILD_ERROR;
+   
+   rest = child->path + i;
+   if(strlen(child->path) >= i && rest[0] != '/')
+      return PARENT_CHILD_ERROR;
+   
+   rest++;
+   if(strstr(rest, "/") != NULL)
+      return PARENT_CHILD_ERROR;
+   
+   child->parent = parent;
+
+   if(DynArray_bsearch(parent->dchildren, child, &i,
+         (int (*)(const void*, const void*)) Node_compare) == 1)
+      return ALREADY_IN_TREE;
+
+    if(DynArray_bsearch(parent->dchildren, child, &i,
+         (int (*)(const void*, const void*)) Node_compare) == 1)
+      return ALREADY_IN_TREE;
+
+   if(DynArray_addAt(parent->dchildren, i, child) == TRUE)
+      return SUCCESS;
+   else
+      return PARENT_CHILD_ERROR;
+}
+
+/* see node.h for specification */
+void Node_unlinkChild(Node_T parent, Node_T child) {
+   size_t i;
+
+   assert(parent != NULL);
+   assert(child != NULL);
+
+   if(DynArray_bsearch(parent->dchildren, child, &i,
+         (int (*)(const void*, const void*)) Node_compare) != 0)
+        (void) DynArray_removeAt(parent->dchildren, i);
+}
+
+
+/* see node.h for specification */
+char* Node_toString(Node_T n) {
+   char* copyPath;
+
+   assert(n != NULL);
+
+   copyPath = malloc(strlen(n->path)+1);
+   if(copyPath == NULL) {
+      return NULL;
+   }
+   else {
+      return strcpy(copyPath, n->path);
+   }
+}
+
+/* see FT_file.h for specification */
+File_T File_create(const char* dir, Node_T parent, void* contents,
+                   size_t length, int* status)
+{
+   File_T new;
+   char* path;
+
+   assert(dir != NULL);
+
+   new = malloc(sizeof(struct file));
+   if(new == NULL) {
+      *status = MEMORY_ERROR;
+      return NULL;
+   }
+
+   if(parent == NULL)
+      path = malloc(strlen(dir)+1);
+   else
+      path = malloc(strlen(parent->path) + 1 + strlen(dir) + 1);
+
+   if(path == NULL) {
+      free(new);
+      *status = MEMORY_ERROR;
+      return NULL;
+   }
+
+   *path = '\0';
+
+   if(parent != NULL) {
+      strcpy(path, parent->path);
+      strcat(path, "/");
+   }
+   strcat(path, dir);
+
+   new->path = path;
+
+   new->parent = parent;
+
+   new->contents = malloc(length);
+
+   if(new->contents == NULL) {
+      free(new->path);
+      free(new);
+      *status = MEMORY_ERROR;
+      return NULL;
+   }
+
+   memcpy(new->contents, contents, length);
+
+   /* if (parent != NULL) {
+      result = Node_linkChild(parent, new, file);
+      if(result != SUCCESS)
+         (void) File_destroy(new, file);
+      *status = result;
+      } */
+
+   return new;
+}
+
+/* see FT_file.h for specification */
+void File_destroy(File_T n) {
+   assert(n != NULL);
+
+   /* if (n->parent != NULL) {
+      File_unlinkChild(n->parent, n);
+      } */
+
+   free(n->path);
+   free(n);
+}
+
+/* see FT_file.h for specification */
+char* File_getPath(File_T n) {
+   char* pathCopy;
+
+   assert(n != NULL);
+
+   pathCopy = malloc(strlen(n->path)+1);
+   if(pathCopy == NULL)
+      return NULL;
+
+   return strcpy(pathCopy, n->path);
+}
+
+
+/* see FT_file.h for specification */
+int File_compare(File_T File1, File_T File2) {
+   assert(File1 != NULL);
+   assert(File2 != NULL);
+
+   return strcmp(File1->path, File2->path);
+}
+
+/* see FT_file.h for specification */
+Node_T File_getParent(File_T n) {
+   assert(n != NULL);
+
+   return n->parent;
+}
+
+/* see FT_file.h for specification */
+void* File_getContents(File_T n) {
+   void* contents;
+
+   assert(n != NULL);
+
+   contents = malloc(n->length);
+   if (contents == NULL)
+      return NULL;
+
+   memcpy(contents, n->contents, n->length);
+   return contents;
+}
+
+/* see FT_file.h for specification */
+void* File_replaceContents(File_T n, void *contents, size_t length) {
+   void* original;
+   assert(n != NULL);
+
+   original = n->contents;
+
+   n->contents = malloc(length);
+   if (n->contents == NULL)
+      return NULL;
+
+   memcpy(n->contents, contents, length);
+   return original;
+}
+
+/* see node.h for specification */
+int File_linkChild(Node_T parent, File_T child) {
+   size_t i;
+   char* rest;
+
+   assert(parent != NULL);
+   assert(child != NULL);
+
+   if(Node_hasChild(parent, child->path, NULL, TRUE))
+      return ALREADY_IN_TREE;
+
+   i = strlen(parent->path);
+   if(strncmp(child->path, parent->path, i))
+      return PARENT_CHILD_ERROR;
+
+   rest = child->path + i;
+   if(strlen(child->path) >= i && rest[0] != '/')
+      return PARENT_CHILD_ERROR;
+
+   rest++;
+   if(strstr(rest, "/") != NULL)
+      return PARENT_CHILD_ERROR;
+
+   child->parent = parent;
+
+   if(DynArray_bsearch(parent->fchildren, child, &i,
+                       (int (*)(const void*, const void*)) File_compare) == 1)
+      return ALREADY_IN_TREE;
+
+   if(DynArray_bsearch(parent->fchildren, child, &i,
+                       (int (*)(const void*, const void*)) File_compare) == 1)
+      return ALREADY_IN_TREE;
+
+   if(DynArray_addAt(parent->fchildren, i, child) == TRUE)
+      return SUCCESS;
+   else
+      return PARENT_CHILD_ERROR;
+}
+
+/* see node.h for specification */
+void File_unlinkChild(Node_T parent, File_T child) {
+   size_t i;
+
+   assert(parent != NULL);
+   assert(child != NULL);
+
+   if(DynArray_bsearch(parent->fchildren, child, &i,
+                       (int (*)(const void*, const void*)) File_compare) != 0)
+      (void) DynArray_removeAt(parent->fchildren, i);
+}
+
+/* see FT_file.h for specification */
+char* File_ToString(File_T n) {
+   char* copyPath;
+
+   assert(n != NULL);
+
+   copyPath = malloc(strlen(n->path)+1);
+   if(copyPath == NULL) {
+      return NULL;
+   }
+   else {
+      return strcpy(copyPath, n->path);
+   }
+}
---
+++ 3FT/node.h
@@ -0,0 +1,198 @@
+/*--------------------------------------------------------------------*/
+/* node.h                                                             */
+/* Author: Shruti Roy, Misrach Ewunetie                               */
+/*--------------------------------------------------------------------*/
+
+#ifndef NODE_INCLUDED
+#define NODE_INCLUDED
+
+#include <stddef.h>
+#include "a4def.h"
+
+/*
+   a Node_T is an object that contains a path payload and references to
+   the node's parent (if it exists) and children (if they exist).
+   Children may be either files, or other node directories.
+*/
+typedef struct node* Node_T;
+
+/*
+   a File_T is an object that contains a path payload and references to
+   the File's parent (if it exists) and children (if they exist).
+*/
+typedef struct file* File_T;
+
+/*
+   Given a parent node and a directory string dir, returns a new
+   Node_T or NULL if any allocation error occurs in creating
+   the node or its fields.
+
+   The new structure is initialized to have its path as the parent's
+   path (if it exists) prefixed to the directory string parameter,
+   separated by a slash. It is also initialized with its parent link
+   as the parent parameter value, and the parent itself is changed
+   to link to the new node.  The children links are initialized but
+   do not point to any children. status is changed to reflect the result
+   of the function, i.e. SUCCESS if successful, ALREADY_IN_TREE if a 
+   directory already exists at that path, MEMORY_ERROR if unable
+   to allocate some memory, and PARENT_CHILD_ERROR if unable to link
+   the parent to its child.
+*/
+Node_T Node_create(const char* dir, Node_T parent, int *status);
+
+/*
+  Destroys the entire hierarchy of nodes rooted at n,
+  including n itself.
+*/
+size_t Node_destroy(Node_T n);
+
+/*
+  Compares node1 and node2 based on their paths.
+  Returns <0, 0, or >0 if node1 is less than,
+  equal to, or greater than node2, respectively.
+*/
+int Node_compare(Node_T node1, Node_T node2);
+
+/*
+   Returns n's path.
+*/
+char* Node_getPath(Node_T n);
+
+/*
+  Returns the number of child directories n has.
+*/
+size_t Node_getNumChildren(Node_T n, boolean file);
+
+/*
+   Returns 1 if n has a child directory with path,
+   0 if it does not have such a child, and -1 if
+   there is an allocation error during search.
+
+   If n does have such a child, and childID is not NULL, store the
+   child's identifier in *childID. If n does not have such a child,
+   store the identifier that such a child would have in *childID.
+*/
+int Node_hasChild(Node_T n, const char* path, size_t* childID,
+                  boolean file);
+
+/*
+   Returns the child node of n with identifier childID, if one exists,
+   otherwise returns NULL.
+*/
+Node_T Node_getDirChild(Node_T n, size_t childID);
+
+/* Returns the child file of n with identifier childID, if one exists,
+   otherwise returns NULL.
+*/
+File_T Node_getFileChild(Node_T n, size_t childID);
+
+/*
+   Returns the parent node of n, if it exists, otherwise returns NULL
+*/
+Node_T Node_getParent(Node_T n);
+
+/*
+  Makes child a child of parent, if possible, and returns SUCCESS.
+  This is not possible in the following cases:
+  * parent already has a child with child's path,
+    in which case: returns ALREADY_IN_TREE
+    * child's path is not parent's path + / + directory,
+    or the parent cannot link to the child,
+    in which cases: returns PARENT_CHILD_ERROR
+*/
+int Node_linkChild(Node_T parent, Node_T child);
+
+/*
+  Unlinks node parent from its child node child, if it can be found in 
+  the parent's children. child is unchanged.
+*/
+void Node_unlinkChild(Node_T parent, Node_T child);
+
+/*
+  Returns a string representation for n, 
+  or NULL if there is an allocation error.
+
+  Allocates memory for the returned string,
+  which is then owned by client!
+*/
+char* Node_toString(Node_T n);
+
+/*
+   Given a parent File and a directory string dir, returns a new
+   File_T or NULL if any allocation error occurs in creating
+   the File or its fields.
+   The new structure is initialized to have its path as the parent's
+   path (if it exists) prefixed to the directory string parameter,
+   separated by a slash. It is also initialized with its parent link
+   as the parent parameter value, and the parent itself is changed
+   to link to the new File. status is changed to the result of the
+   function, i.e. SUCCESS if successful, ALREADY_IN_TREE if the file
+   already exists at that path, MEMORY_ERROR if it cannot allocate some
+   memory, and PARENT_CHILD_ERROR if a parent cannot link to the child.
+*/
+
+File_T File_create(const char* dir, Node_T parent, void* contents,
+                   size_t length, int *status);
+
+/*
+  Destroys the file n.
+*/
+void File_destroy(File_T n);
+
+
+/*
+  Compares File1 and File2 based on their paths.
+  Returns <0, 0, or >0 if File1 is less than
+  equal to, or greater than File2, respectively.
+*/
+int File_compare(File_T File1, File_T File2);
+
+/*
+   Returns n's path.
+*/
+char* File_getPath(File_T n);
+
+/*
+   Returns the parent Node of n, if it exists, otherwise returns NULL
+*/
+Node_T File_getParent(File_T n);
+
+/* Returns a copy of the content contained within the File of n, 
+   if it exists, otherwise returns NULL. The caller will then own the 
+   memory allocated for the copy of the content of the file 
+*/
+void* File_getContents(File_T n);
+
+/* Replaces the content of the file n with the contents passed in using
+   the length of the contents in order to replace the contents.
+   Returns a copy of the contents of file n, for which the client is
+   now responsible.
+*/
+void* File_replaceContents(File_T n, void *contents, size_t length);
+
+/*
+  Makes child a child of parent, if possible, and returns SUCCESS.
+  This is not possible in the following cases:
+  * parent already has a child with child's path,
+    in which case: returns ALREADY_IN_TREE
+    * child's path is not parent's path + / + directory,
+    or the parent cannot link to the child,
+    in which cases: returns PARENT_CHILD_ERROR
+*/
+int File_linkChild(Node_T parent, File_T child);
+
+/*
+  Unlinks File parent from its child File child, if it can be found in 
+  the parent's children. child is unchanged.
+*/
+void File_unlinkChild(Node_T parent, File_T child);
+
+/*
+  Returns a string representation for n, 
+  or NULL if there is an allocation error.
+  Allocates memory for the returned string,
+  which is then owned by client!
+*/
+char* File_ToString(File_T n);
+
+#endif
