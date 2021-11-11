3d113658b37f717e8752b8b68f6f2686d5fa353f
mewunetie <90476983+mewunetie@users.noreply.github.com>
2021-11-11 16:13:14 -0500

    Update ft.c

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -740,6 +740,10 @@ char *FT_toString(void)
    if(!isInitialized)
       return NULL;
 
+   if (root == NULL) {
+      return "";
+   }
+
    nodes = DynArray_new(count);
    if (nodes == NULL) {
       return NULL;
